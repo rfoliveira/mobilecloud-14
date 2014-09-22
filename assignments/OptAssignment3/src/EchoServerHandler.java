41894c5d8f41b81605f1eae0bea9d97cbbb267f6
Rafael Fernandes de Oliveira <rafaelolivei@gmail.com>
2014-09-22 08:43:45 -0300

    Optional assignment 3 (Java)
    Tentando concluir o exercício com Netty.

--- assignments/OptAssignment3/src/EchoServerHandler.java
+++ assignments/OptAssignment3/src/EchoServerHandler.java
@@ -1,32 +1,42 @@
 import java.net.InetSocketAddress;
-import java.net.SocketAddress;
 import java.util.concurrent.Executors;
 
-import org.jboss.netty.bootstrap.Bootstrap;
 import org.jboss.netty.bootstrap.ServerBootstrap;
 import org.jboss.netty.buffer.ChannelBuffer;
 import org.jboss.netty.channel.Channel;
 import org.jboss.netty.channel.ChannelEvent;
 import org.jboss.netty.channel.ChannelFactory;
 import org.jboss.netty.channel.ChannelHandlerContext;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
 import org.jboss.netty.channel.ChannelStateEvent;
+import org.jboss.netty.channel.Channels;
 import org.jboss.netty.channel.ExceptionEvent;
 import org.jboss.netty.channel.MessageEvent;
 import org.jboss.netty.channel.SimpleChannelUpstreamHandler;
 import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
 
+/*
+ * Create an EchoServerHandler that inherits from SimpleChannelUpstreamHandler 
+ * and implement its messageReceived() hook method so that it echos back the client's input 
+ * either (a) a "chunk" at a time or (b) a "line" at a time (i.e., until the symbols "\n", "\r", or "\r\n" are read), 
+ * rather than a character at a time.   
+ * You can additionally override methods such as channelOpen for example to show logging information.
+ */
 public class EchoServerHandler extends SimpleChannelUpstreamHandler {
 
 	@Override
 	public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) {
 		ChannelBuffer buf = (ChannelBuffer)e.getMessage();
 		
+		Channel ch = e.getChannel();
+		ch.write(e.getMessage());		
+		
 		while (buf.readable()) {
-			if((buf.readChar() == '\n') || (buf.readChar() == '\r'))
-				System.out.println(buf.readChar());
-			
+			char c = (char)buf.readByte();
+			System.out.println(c);
 			System.out.flush();
-		}		
+		}
 	}
 	
 	@Override
@@ -39,12 +49,12 @@ public class EchoServerHandler extends SimpleChannelUpstreamHandler {
 		
 		super.handleUpstream(ctx, e);		
 	}
-	
-	@Override
-	public void channelOpen(ChannelHandlerContext ctx, ChannelStateEvent e)
-			throws Exception {
-		
-	}
+//	
+//	@Override
+//	public void channelOpen(ChannelHandlerContext ctx, ChannelStateEvent e)
+//			throws Exception {
+//		
+//	}
 	
 	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) {
@@ -56,11 +66,35 @@ public class EchoServerHandler extends SimpleChannelUpstreamHandler {
 	
 	public static class NettyServer {
 		public void startServer() throws Exception {
+			// Configure the ServerSocketChannelFactory that inherits from ServerChannelFactory.
 			ChannelFactory factory = new NioServerSocketChannelFactory(
 					Executors.newCachedThreadPool(),
 					Executors.newCachedThreadPool());
 			
+			// Configure the ServerBootstrap class with appropriate thread pools to run the events
 			ServerBootstrap bootstrap = new ServerBootstrap(factory);
+			
+			// Configure the bootstrap object with a PipelineFactory
+			bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+				
+				/*
+				 * Create a ChannelPipelineFactory which sets up a pipeline for processing the inbound messages.
+				 * @see org.jboss.netty.channel.ChannelPipelineFactory#getPipeline()
+				 */
+				@Override
+				public ChannelPipeline getPipeline() throws Exception {
+					/*
+					 * When a client connection request arrives, the ServerChannelFactory and ChannelPipeline 
+					 * work together to pass events to the EchoServerHandler.
+					 */
+					return Channels.pipeline(new EchoServerHandler());					
+				}
+			});
+			
+			bootstrap.setOption("tcpNoDelay", true);
+			bootstrap.setOption("keepAlive", true);
+			
+			// binds the ServerBootstrap class to an appropriate InetSocketAddress
 			bootstrap.bind(new InetSocketAddress(80));
 		}		
 	}
